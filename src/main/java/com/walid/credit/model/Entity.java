0c58983a6d7b2378466ad729152febd8d71b0971
Walid Moustafa <waleedaleem@hotmail.com>
2020-09-08 22:54:39 +1000

    Implement Entity class equals() and hashcode()

--- src/main/java/com/walid/credit/model/Entity.java
+++ src/main/java/com/walid/credit/model/Entity.java
@@ -1,7 +1,7 @@
 package com.walid.credit.model;
 
-import java.math.BigDecimal;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * @author Walid Moustafa
@@ -11,8 +11,60 @@ public class Entity {
     private String id;
     private Entity parent;
     private List<Entity> children;
-    private BigDecimal limit;
-    private BigDecimal childTotalLimit;
-    private BigDecimal directUtilisation;
-    private BigDecimal totalUtilisation;
+    private long limit;
+    private long childTotalLimit;
+    private long utilisation;
+    private long totalUtilisation;
+    private boolean isLoaded;
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setParent(Entity parent) {
+        this.parent = parent;
+    }
+
+    public void setChildren(List<Entity> children) {
+        this.children = children;
+    }
+
+    public void setLimit(long limit) {
+        this.limit = limit;
+    }
+
+    public void setChildTotalLimit(long childTotalLimit) {
+        this.childTotalLimit = childTotalLimit;
+    }
+
+    public void setUtilisation(long utilisation) {
+        this.utilisation = utilisation;
+    }
+
+    public void setTotalUtilisation(long totalUtilisation) {
+        this.totalUtilisation = totalUtilisation;
+    }
+
+    public void setLoaded() {
+        isLoaded = true;
+    }
+
+    public boolean alreadyLoaded() {
+        return isLoaded;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Entity entity = (Entity) o;
+        return id.equals(entity.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
